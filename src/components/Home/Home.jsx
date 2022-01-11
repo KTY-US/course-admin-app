b6993062df35ce4809ef0df81d89afbd25595907
tinbui1908 <bhtt190800@gmail.com>
2022-01-11 22:10:40 +0700

    add dashboard layout

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 
+import DashboardLayoutRoot from '../Dashboard/dashboard-layout';
+
 const Home = () => {
-	return <div>Hello</div>;
+	return <DashboardLayoutRoot />;
 };
 
 export default Home;
