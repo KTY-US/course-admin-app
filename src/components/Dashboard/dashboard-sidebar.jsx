1436749844fb663c87cd3423bd5d835bcddb0c0a
tinbui1908 <bhtt190800@gmail.com>
2022-01-13 19:01:36 +0700

    edit layout for sidebar

--- src/components/Dashboard/dashboard-sidebar.jsx
+++ src/components/Dashboard/dashboard-sidebar.jsx
@@ -1,59 +1,26 @@
 import React from 'react';
-import { Box, Button, Divider, Drawer, Typography, useMediaQuery } from '@mui/material';
-import OpenInNewIcon from '@mui/icons-material/OpenInNew';
-import { ChartBar as ChartBarIcon } from '../../icons/chart-bar';
-import { Cog as CogIcon } from '../../icons/cog';
-import { Lock as LockIcon } from '../../icons/lock';
-import { Selector as SelectorIcon } from '../../icons/selector';
-import { ShoppingBag as ShoppingBagIcon } from '../../icons/shopping-bag';
-import { User as UserIcon } from '../../icons/user';
-import { UserAdd as UserAddIcon } from '../../icons/user-add';
-import { Users as UsersIcon } from '../../icons/users';
-import { XCircle as XCircleIcon } from '../../icons/x-circle';
-import { Logo } from './logo';
-import { NavItem } from './nav-item';
 import { Link } from 'react-router-dom';
+import { Box, Drawer, Typography, useMediaQuery } from '@mui/material';
+import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
+import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
+import { NavItem } from './nav-item';
+import SchoolIcon from '@mui/icons-material/School';
 
 const items = [
 	{
 		href: '/',
-		icon: <ChartBarIcon fontSize='small' />,
-		title: 'Dashboard'
+		icon: <ManageAccountsIcon fontSize='small' />,
+		title: 'Admins'
 	},
 	{
 		href: '/customers',
-		icon: <UsersIcon fontSize='small' />,
-		title: 'Customers'
+		icon: <SupervisorAccountIcon fontSize='small' />,
+		title: 'Users'
 	},
 	{
 		href: '/products',
-		icon: <ShoppingBagIcon fontSize='small' />,
-		title: 'Products'
-	},
-	{
-		href: '/account',
-		icon: <UserIcon fontSize='small' />,
-		title: 'Account'
-	},
-	{
-		href: '/settings',
-		icon: <CogIcon fontSize='small' />,
-		title: 'Settings'
-	},
-	{
-		href: '/login',
-		icon: <LockIcon fontSize='small' />,
-		title: 'Login'
-	},
-	{
-		href: '/register',
-		icon: <UserAddIcon fontSize='small' />,
-		title: 'Register'
-	},
-	{
-		href: '/404',
-		icon: <XCircleIcon fontSize='small' />,
-		title: 'Error'
+		icon: <SchoolIcon fontSize='small' />,
+		title: 'Courses'
 	}
 ];
 
@@ -106,9 +73,15 @@ export const DashboardSidebar = (props) => {
 				<br />
 				<br />
 
-				<Box sx={{ flexGrow: 1 }}>
+				<Box sx={{ flexGrow: 1, px: 1 }}>
 					{items.map((item) => (
-						<NavItem key={item.title} icon={item.icon} href={item.href} title={item.title} />
+						<NavItem
+							key={item.title}
+							icon={item.icon}
+							href={item.href}
+							title={item.title}
+							sx={{ marginBottom: '5px' }}
+						/>
 					))}
 				</Box>
 			</Box>
