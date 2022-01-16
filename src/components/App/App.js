810e24044c8fc7dde616c7dc7e088374bf36f004
Hoàng Khang <nhk25022016@gmail.com>
2022-01-16 14:22:11 +0700

    wrap App component with redux Provider, add course reducer into redux index file

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { Container } from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
@@ -10,26 +12,33 @@ import Account from '../Account/Account';
 import Login from '../Auth/login';
 import Courses from '../Courses/Courses';
 import DashboardLayoutRoot from '../Dashboard/dashboard-layout';
+import Course from '../Courses/Course/Course';
 
+import store from '../../reducers';
 const theme = createTheme();
 const App = () => {
 	return (
 		<BrowserRouter>
-			<ThemeProvider theme={theme}>
-				<DashboardLayoutRoot>
-					<Container>
-						<Routes>
-							<Route path='/' element={<Navigate to='/courses' />} />
-							<Route path='/courses' element={<Courses />} />
-							<Route path='/auth/signin' element={<Login />} />
-							<Route path='/user' element={<Account />} />
-							<Route path='/not-found' element={<Error content={'404 Page not found'} />} />
-							<Route path='*' element={<Error content={'404 Page not found'} />}></Route>
-						</Routes>
-					</Container>
-					<ToastContainer autoClose={5000} />
-				</DashboardLayoutRoot>
-			</ThemeProvider>
+			<Provider store={store}>
+				<ThemeProvider theme={theme}>
+					<DashboardLayoutRoot>
+						<Container>
+							<Routes>
+								<Route path='/' element={<Navigate to='/courses' />} />
+								<Route path='/courses'>
+									<Route index element={<Courses />} />
+									<Route path=':courseId' element={<Course />} />
+								</Route>
+								<Route path='/auth/signin' element={<Login />} />
+								<Route path='/user' element={<Account />} />
+								<Route path='/not-found' element={<Error content={'404 Page not found'} />} />
+								<Route path='*' element={<Error content={'404 Page not found'} />}></Route>
+							</Routes>
+						</Container>
+						<ToastContainer autoClose={5000} />
+					</DashboardLayoutRoot>
+				</ThemeProvider>
+			</Provider>
 		</BrowserRouter>
 	);
 };
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import authSlice from './auth';
+import courseSlice from './course';
 
 const store = configureStore({
-	reducer: { auth: authSlice.reducer }
+	reducer: { auth: authSlice.reducer, course: courseSlice.reducer }
 });
 
 export default store;
