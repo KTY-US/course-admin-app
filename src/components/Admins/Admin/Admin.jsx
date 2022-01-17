fc59a2b28c96c0241e8bdcd3ead158aca5ff7cd6
tinbui1908 <bhtt190800@gmail.com>
2022-01-17 10:15:25 +0700

    add admins UI

---
+++ src/components/Admins/Admin/Admin.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { TableCell, TableRow } from '@mui/material';
+
+const User = ({ user, stt }) => {
+	const navigate = useNavigate();
+	const openUser = () => {
+		navigate(`/users/${user.id}`);
+	};
+	const getFullName = (firstName, lastName) => {
+		const fullName = firstName + ' ' + lastName;
+		if (fullName.length > 60) return fullName.slice(0, 60) + '...';
+
+		return fullName;
+	};
+	return (
+		<>
+			{user && (
+				<TableRow hover role='checkbox' onClick={openUser} style={{ cursor: 'pointer' }}>
+					<TableCell component='th' scope='row' align='center'>
+						{stt}
+					</TableCell>
+
+					<TableCell>{getFullName(user.firstName, user.lastName)}</TableCell>
+					<TableCell>{user.email}</TableCell>
+					<TableCell align='center'>{user.userCode}</TableCell>
+				</TableRow>
+			)}
+		</>
+	);
+};
+
+export default User;
---
+++ src/components/Admins/AdminDetail/UserDetail.jsx
@@ -0,0 +1,79 @@
+import React, { useState, useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { Box, Container, Grid, Typography, Button, CircularProgress } from '@mui/material';
+import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
+
+import AccountProfile from './AccountProfile';
+import AccountProfileDetails from './AccountProfileDetails';
+import { getUser } from '../../../apis/user';
+
+const AdminDetail = () => {
+	const navigate = useNavigate();
+	const { id: userId } = useParams();
+	const [userData, setUserData] = useState({});
+	const [isLoading, setIsLoading] = useState(false);
+
+	const handleGoBack = () => {
+		navigate(-1);
+	};
+
+	useEffect(async () => {
+		setIsLoading(true);
+		const { data } = await getUser(userId);
+		setUserData(data);
+		setIsLoading(false);
+	}, []);
+
+	return (
+		<>
+			{isLoading ? (
+				<Grid container marginTop={5} maxWidth='100%' display='flex' justifyContent='center'>
+					<CircularProgress />
+				</Grid>
+			) : (
+				<>
+					{userData && (
+						<Box
+							component='main'
+							sx={{
+								flexGrow: 1,
+								py: 3
+							}}
+						>
+							<Container maxWidth='lg'>
+								<Button
+									sx={{ textTransform: 'none', display: 'flex', alignItems: 'center' }}
+									onClick={handleGoBack}
+								>
+									<KeyboardBackspaceIcon fontSize='small' /> Go back
+								</Button>
+								<Typography
+									sx={{
+										mb: 3,
+										mt: 1,
+										display: 'flex',
+										alignItems: 'center',
+										justifyContent: 'center'
+									}}
+									variant='h4'
+								>
+									Account
+								</Typography>
+								<Grid container spacing={3}>
+									<Grid item lg={4} md={6} xs={12}>
+										<AccountProfile user={userData} />
+									</Grid>
+									<Grid item lg={8} md={6} xs={12}>
+										<AccountProfileDetails user={userData} />
+									</Grid>
+								</Grid>
+							</Container>
+						</Box>
+					)}
+				</>
+			)}
+		</>
+	);
+};
+
+export default AdminDetail;
